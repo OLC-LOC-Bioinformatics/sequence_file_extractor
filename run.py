20a0b5ad52078f50eb6c003ca25412e377241466
devonpmack <devonpmack@gmail.com>
2017-05-25 15:41:17 -0400

    Choose output folder path

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ import zipfile
 import argparse
 
 parser = argparse.ArgumentParser(description="Retrieve fasta and/or fastq files from the NAS")
-parser.add_argument('output_folder_name', type=str, help="Name of the folder to put the result files in")
+parser.add_argument('output_folder', type=str, help="Folder to put the results files in")
 parser.add_argument("-f", "--fasta", action="store_true",
                     help="Retrieve fasta files in fasta_retrieve_list.txt")
 parser.add_argument("-q", "--fastq", action="store_true",
@@ -30,23 +30,10 @@ if not args.fastq and not args.fasta:
 script_dir = sys.path[0]
 
 # Load NAS directory
-load = SaveLoad()
-if load.load(file_name="config.json", create=True):
-    if 'nasmnt' in load.__dict__:
-        nasmnt = load.nasmnt
-    else:
-        load.nasmnt = input("nasmnt not in config.json, please enter it here:\n")
-        nasmnt = load.nasmnt
-        load.dump('config.json')
-        print("Updated config.json")
-else:
-    # File created
-    load.nasmnt = input("nasmnt not in config.json, please enter it here:\n")
-    nasmnt = load.nasmnt
-    load.dump('config.json')
-    print("Updated config.json")
-
-retriever = SequenceGetter(outputfolder=os.path.join(script_dir, 'extract', name),
+load = SaveLoad(file_name="config.json", create=True)
+nasmnt = load.get('nasmnt', default='/mnt/nas/')
+outfolder = os.path.normpath(name)
+retriever = SequenceGetter(outputfolder=outfolder,
                            nasmnt=os.path.normpath(nasmnt), output=False)
 
 t = Timer()
@@ -60,7 +47,7 @@ if args.fastq:
     f.close()
 
     for seq_id in ids:
-        for r in [1,2]:
+        for r in [1, 2]:
             t.time_print(retriever.retrieve_file(seq_id, filetype="fastq_R%d" % r))
 
 if args.fasta:
@@ -74,7 +61,7 @@ if args.fasta:
 
 if args.zip:
     # Zip all the files
-    p = os.path.join(script_dir, 'extract', name)
+    p = outfolder
     results_zip = os.path.join(p, name + '.zip')
     t.time_print("Creating zip file %s" % results_zip)
 
